c918505cb6b52375b86f2f8f457be3740393f8f6
HamzaIK <hamzadahouik@gmail.com>
2024-05-10 01:55:33 +0100

    favicon and scrollbar

--- containers/Home.tsx
+++ containers/Home.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+'use client'
+import {useEffect} from 'react'
 import Header from '@/components/Header'
 import Pass from '@/components/Pass'
 import Profiting from '@/components/Profiting'
 import Vision from '@/components/Vision'
 import Cards from '@/components/Cards'
 import RoadMap from '@/components/RoadMap'
-const page = () => {
+
+
+const Home = () => {
+
+  useEffect(() => {
+
+    (
+      async () => {
+        const LocomotiveScroll = (await import('locomotive-scroll')).default
+        const locomotiveScroll = new LocomotiveScroll();
+      }
+    )()
+
+  }, []);
+
   return (
     <div>
       
@@ -23,4 +38,4 @@ const page = () => {
   )
 }
 
-export default page
+export default Home
